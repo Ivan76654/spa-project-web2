598590f008e6a66275c208fccd5161b0ae20459a
Ivan76654 <ivan.lovric2101@gmail.com>
2024-01-23 17:00:22 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router';
+
+export default {
+  name: 'App'
+};
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: 'Home'
+};
+</script>
+
+<template>
+  <main>
+    <p>Home</p>
+  </main>
+</template>
